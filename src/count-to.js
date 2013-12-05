f9e32bc86d033894ee12011ffdc9e7a4d1305dc8
Francesco Nistri <francesconistri84@gmail.com>
2013-12-05 17:44:02 +0100

    fix directive to use with firefox

--- src/count-to.js
+++ src/count-to.js
@@ -1,62 +1,61 @@
 var countTo = angular.module('countTo', [])
-    .directive('countTo', ['$timeout', function ($timeout) {
+    .directive('countTo', ['$timeout', function($timeout) {
         return {
             replace: false,
             scope: true,
-            link: function (scope, element, attrs) {
-
-                var e = element[0];
+            link: function(scope, element, attrs) {
                 var num, refreshInterval, duration, steps, step, countTo, value, increment;
 
-                var calculate = function () {
+                var calculate = function() {
                     refreshInterval = 30;
                     step = 0;
                     scope.timoutId = null;
-                    countTo = parseInt(attrs.countTo) || 0;
+                    countTo = parseInt(attrs.countTo, 10) || 0;
                     scope.value = parseInt(attrs.value, 10) || 0;
                     duration = (parseFloat(attrs.duration) * 1000) || 0;
 
                     steps = Math.ceil(duration / refreshInterval);
                     increment = ((countTo - scope.value) / steps);
                     num = scope.value;
-                }
+                };
 
-                var tick = function () {
-                    scope.timoutId = $timeout(function () {
+                var tick = function() {
+                    scope.timoutId = $timeout(function() {
                         num += increment;
                         step++;
                         if (step >= steps) {
                             $timeout.cancel(scope.timoutId);
                             num = countTo;
-                            e.innerText = countTo;
+                            element.html(countTo);
+
                         } else {
-                            e.innerText = Math.round(num);
+                            element.html(Math.round(num));
                             tick();
                         }
                     }, refreshInterval);
 
-                }
+                };
 
-                var start = function () {
+                var start = function() {
                     if (scope.timoutId) {
                         $timeout.cancel(scope.timoutId);
                     }
                     calculate();
                     tick();
-                }
+                };
 
-                attrs.$observe('countTo', function (val) {
+                attrs.$observe('countTo', function(val) {
                     if (val) {
                         start();
                     }
                 });
 
-                attrs.$observe('value', function (val) {
+                attrs.$observe('value', function(val) {
                     start();
                 });
 
                 return true;
             }
-        }
+        };
 
     }]);
